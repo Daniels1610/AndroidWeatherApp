96679a280a803446e845591ca7a7bb467bc13124
Daniels1610 <danielagraz1610@hotmail.com>
2023-05-01 02:45:09 -0700

    Add the corresponding JSONObject variables to access the weather values from the request to WeatherAPI

--- app/src/main/java/activities/MainActivity.java
+++ app/src/main/java/activities/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.bumptech.glide.Glide;
 import com.example.weatherapp.R;
 import com.example.weatherapp.databinding.ActivityMainBinding;
 
@@ -19,11 +20,22 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+// Made by: Daniel Agraz Vallejo. May 1st 2023.
+
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
 
+    // Setting variables for later use
     String currentLocation;
+
+    String latitude;
+    String longitude;
+    String url;
+
+    String minTemp;
+    String currentTemp;
+    String maxTemp;
 
     private final String TAG = "AGRAZ DEBUGGING";
 
@@ -35,16 +47,23 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
 
-        currentLocation = intent.getStringExtra("LATITUDE") + ","
-                + intent.getStringExtra("LONGITUDE");
+        // Improve the readiness of these variables in CurrentLocation
+        latitude = intent.getStringExtra("LATITUDE");
+        longitude = intent.getStringExtra("LONGITUDE");
+        currentLocation = latitude + "," + longitude;
         Log.v(TAG, currentLocation);
 
         final OkHttpClient client = new OkHttpClient();
 
+        // This was the naughty one. I had to double checked that the URL that we were sending the request was about right
         final Request request = new Request.Builder()
-                .url(Network.openWeatherAPI + "current.json?key=" + Network.openWeatherAPIKey
-                        + "&aqi=no&q=" + currentLocation)
+                .url(Network.openWeatherAPI + "forecast.json?key=" + Network.openWeatherAPIKey
+                        + "&q=" + currentLocation + "&days=1&aqi=no&alerts=no")
                 .build();
+
+        // To make sure that the Call to the API was correct
+        url = Network.openWeatherAPI + "forecast.json?key=" + Network.openWeatherAPIKey + "&q=" + currentLocation + "&days=1&aqi=no&alerts=no";
+        Log.d(TAG, url);
 
         @SuppressLint("StaticFieldLeak")
         AsyncTask<Void, Void, String> asyncTask = new AsyncTask<Void, Void, String>() {
@@ -65,12 +84,36 @@ public class MainActivity extends AppCompatActivity {
             @Override
             protected void onPostExecute(String s){
                 super.onPostExecute(s);
+
                 if(s != null) {
                     JSONObject jsonResponse = null;
                     try{
                         jsonResponse = new JSONObject(s);
+
+                        // Getting the JSON Object according to an specific key
                         JSONObject locationObject = jsonResponse.getJSONObject("location");
+                        JSONObject currentObject = jsonResponse.getJSONObject("current");
+                        JSONObject forecastObject = jsonResponse.getJSONObject("forecast").getJSONArray("forecastday").getJSONObject(0);
+                        JSONObject dayObject = forecastObject.getJSONObject("day");
+
+                        // Getting the String from the icon key that has a CDN to an image
+                        String iconObject = currentObject.getJSONObject("condition").getString("icon");
+
+                        // Employing the icon key that has a URL to a PNG image related to the current weather
+                        Glide.with(MainActivity.this)
+                                .load("https:"+iconObject)
+                                .into(binding.weatherImage);
+
+                        // Using placeholders to substitute the value from the JSONObject
+                        minTemp = getString(R.string.min_placeholder, dayObject.getString("mintemp_c"), getString(R.string.celcius));
+                        currentTemp = getString(R.string.current_placeholder, currentObject.getString("temp_c"), getString(R.string.celcius));
+                        maxTemp = getString(R.string.max_placeholder, dayObject.getString("maxtemp_c"), getString(R.string.celcius));
+
+                        // Setting the TextViews according to it's value
                         binding.locationText.setText(locationObject.getString("name"));
+                        binding.minimumText.setText(minTemp);
+                        binding.currentText.setText(currentTemp);
+                        binding.maximumText.setText(maxTemp);
                     } catch (JSONException e){
                         throw new RuntimeException(e);
                     }
@@ -81,3 +124,5 @@ public class MainActivity extends AppCompatActivity {
         asyncTask.execute();
     }
 }
+
+// Made by: Daniel Agraz Vallejo
